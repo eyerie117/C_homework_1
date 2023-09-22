ef960458c2f5d625e780ae3068ed586962a8073e
Kseniia Esepenko <eyerie86@gmail.com>
2023-09-22 13:15:09 +0300

    Task 3

--- Program.cs
+++ Program.cs
@@ -41,3 +41,17 @@ a = -9 b = -3->max = -3
 //     if (b > c) Console.WriteLine($"Максимальное число: {b}");
 //     else Console.WriteLine($"Максимальное число: {c}");
 // }
+
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+// Console.Clear();
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n % 2 == 0) Console.WriteLine("Число четное");
+// else Console.WriteLine("Число нечетное");
