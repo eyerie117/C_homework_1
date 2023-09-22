3340288d5ce584b3dc6d638648c56fdc22850f11
Kseniia Esepenko <eyerie86@gmail.com>
2023-09-22 13:42:00 +0300

    Task 4

--- Program.cs
+++ Program.cs
@@ -55,3 +55,30 @@ a = -9 b = -3->max = -3
 
 // if (n % 2 == 0) Console.WriteLine("Число четное");
 // else Console.WriteLine("Число нечетное");
+
+/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// Вариант 1.
+
+for (int i = 0; i <= N; i++)
+{
+    if (i % 2 == 0) Console.Write(i + " ");
+}
+
+// Вариант 2.
+
+// int i = 2;
+
+// while (i <= N)
+// {
+//     Console.Write(i + " ");
+//     i += 2;
+// }
